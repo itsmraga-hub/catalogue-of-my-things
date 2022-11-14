2c7e3ecbb11fe7a931a05f212da4ffd2eab6e1f2
Raga <ragawilliam570@gmail.com>
2022-11-14 15:42:08 +0300

    Add class Item

---
+++ item.rb
@@ -0,0 +1,28 @@
+class Item
+  def initialize(label, publish_date, archived)
+    @id = Random.rand(1...1000)
+    @label = label
+    @publish_date = publish_date
+    @archived = archived
+  end
+
+  def set_genre(genre)
+
+  end
+
+  def set_author(author)
+
+  end
+
+  def set_source(source)
+
+  end
+
+  def can_be_archived?
+    2022 - @publish_date > 10 ? true : false
+  end
+
+  def move_to_archive!
+    @archived = true if can_be_archived?
+  end
+end
