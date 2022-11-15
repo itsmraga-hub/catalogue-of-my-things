4126e301d1e6f9784639287add06052db78c692b
milen-ale <milenalebel2@gmail.com>
2022-11-15 15:42:06 +0300

    add book and label

---
+++ Book/book.rb
@@ -0,0 +1,15 @@
+require_relative '../item'
+class Book < Item
+  def initialize(publish_date, publisher, cover_state, id = Random.rand(1...1000))
+    super(publish_date, id)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    super || cover_state == 'bad'
+  end
+  attr_accessor :publisher, :cover_state
+end
+
+book = Book.new('22-02-2022', 'RK Publishing House', 'good')
--- item.rb
+++ item.rb
@@ -1,16 +1,15 @@
 class Item
-  def initialize(label, publish_date, archived)
+  def initialize(label, publish_date, archived: true)
     @id = Random.rand(1...1000)
     @label = label
     @publish_date = publish_date
     @archived = archived
   end
 
-  # def set_genre(genre); end
-
-  # def set_author(author); end
-
-  # def set_source(source); end
+  def label=(label)
+    @label = label
+    label.items.push(self) unless label.items.include?(self)
+  end
 
   def can_be_archived?
     2022 - @publish_date > 10
@@ -19,4 +18,5 @@ class Item
   def move_to_archive!
     @archived = true if can_be_archived?
   end
+  attr_accessor :publish_date, :id, :archived
 end
