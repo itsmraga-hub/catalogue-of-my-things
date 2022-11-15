3e44792209692fa199e0cc019175544fe03c8b65
Raga <ragawilliam570@gmail.com>
2022-11-15 14:51:17 +0300

    Add genre class tests

---
+++ spec/genre_spec.rb
@@ -0,0 +1,26 @@
+require '../musicAlbum/genre'
+require '../musicAlbum/music_album'
+
+describe MusicAlbum do
+  context "When testing the Genre class" do
+    it "Should create a new Genre instance with passed properties" do
+      genre = Genre.new('Rock Music')
+      expect(genre.name).to eq 'Rock Music'
+    end
+  end
+
+  context "When testing the Genre class method add_item" do
+    it "Should add a music instance to music_album_items" do
+      genre = Genre.new('Rock Music')
+      m = MusicAlbum.new('Love song', true, 2022)
+      genre.add_item(m)
+      expect(genre.music_album_items.length).to eq 1
+      expect(genre.music_album_items[0]).to eq m
+      m_two = MusicAlbum.new('Love song', true, 2022)
+      genre.add_item(m_two)
+      expect(genre.music_album_items.length).to eq 2
+      expect(genre.music_album_items[1]).to eq m_two
+    end
+  end
+
+end
