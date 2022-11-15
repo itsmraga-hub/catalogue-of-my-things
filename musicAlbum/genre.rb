b985362f55407d7571b47ac716a7cf1fd5365a3e
Raga <ragawilliam570@gmail.com>
2022-11-15 12:16:12 +0300

    Add genre class

---
+++ musicAlbum/genre.rb
@@ -0,0 +1,24 @@
+require_relative './music_album'
+# require 'pry'
+
+class Genre
+  attr_accessor :name
+  attr_reader :music_album_items, :id
+
+  def initialize(name)
+    @id = Random.rand(1...1000)
+    @name = name
+    @music_album_items = []
+  end
+
+  def add_item(item)
+    @music_album_items << item
+    item.genre = self
+  end
+end
+
+g = Genre.new("RnB")
+m = MusicAlbum.new('Love song', true, 2010, false)
+m_two = MusicAlbum.new('Hate song', true, 2020, false)
+
+# binding.pry
